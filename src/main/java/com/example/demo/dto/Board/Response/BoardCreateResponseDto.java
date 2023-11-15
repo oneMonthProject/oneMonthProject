4ae0921affcb1a51ff20694abb04e0fdc285e49e
sunggun1 <readingdeveloper@naver.com>
2023-11-15 19:10:35 +0900

    [feature-3-create-board-entity] BoardCreateResponseDto 업로드

---
+++ src/main/java/com/example/demo/dto/Board/Response/BoardCreateResponseDto.java
@@ -0,0 +1,33 @@
+package com.example.demo.dto.Board.Response;
+
+import com.example.demo.model.Board;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class BoardCreateResponseDto {
+    private String boardTitle;
+    private String boardContent;
+    private long boardPageView;
+    private boolean boardCompleteStatus;
+    private long boardUserId;
+    private String boardContact;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+
+    public static BoardCreateResponseDto of(Board board) {
+        return BoardCreateResponseDto.builder()
+                .boardTitle(board.getTitle())
+                .boardContent(board.getContent())
+                .boardPageView(board.getPageView())
+                .boardCompleteStatus(board.isCompleteStatus())
+                .boardUserId(board.getUser().getId())
+                .boardContact(board.getContent())
+                .createDate(board.getCreateDate())
+                .updateDate(board.getUpdateDate())
+                .build();
+    }
+}
