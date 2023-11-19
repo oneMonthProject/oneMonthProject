ed8b0792c3f67d4dfaead608f6265df046430837
sunggun1 <readingdeveloper@naver.com>
2023-11-19 14:09:08 +0900

    [feature-3-noahDevelop] RestController 추가

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -18,7 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
 public class BoardController {
