0a5d0384e5131edffe7b36cc24571ca048980c6f
joo <rkdepik@gmail.com>
2023-11-26 16:49:25 +0900

    [feature/19-trustscore-api] refactor : 신뢰점수 관련 엔티티 연관관계 해제

--- src/main/java/com/example/demo/controller/trustscore/TrustScoreController.java
+++ src/main/java/com/example/demo/controller/trustscore/TrustScoreController.java
@@ -1,7 +1,26 @@
 package com.example.demo.controller.trustscore;
 
+import com.example.demo.dto.Common.ResponseDto;
+import com.example.demo.dto.trustscore.request.TrustScoreUpdateRequestDto;
+import com.example.demo.dto.trustscore.response.TrustScoreUpdateResponseDto;
+import com.example.demo.service.trustscore.TrustScoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
+@RequiredArgsConstructor
 public class TrustScoreController {
+    private final TrustScoreService trustScoreService;
+    @PostMapping("/api/trust-score")
+    public ResponseEntity<ResponseDto<?>> updateScoreByManager(
+            @RequestBody @Valid TrustScoreUpdateRequestDto requestDto) {
+        TrustScoreUpdateResponseDto responseDto = trustScoreService.updateTrustScore(requestDto);
+        return new ResponseEntity<>(new ResponseDto<>("success", responseDto), HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/demo/model/trustscore/TrustScore.java
+++ src/main/java/com/example/demo/model/trustscore/TrustScore.java
@@ -5,26 +5,43 @@ import com.example.demo.model.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import net.bytebuddy.asm.Advice;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Table(name = "trust_score")
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class TrustScore extends BaseTimeEntity {
+public class TrustScore implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 5571826191135479993L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "trust_score_id")
     private Long id;
+    /**
+     * 유저자동생성식별자
+     */
+    @Column
+    private Long userId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    /**
+     * 유저신뢰점수값
+     */
+    @Column
+    private int score;
 
-    @Column(name = "trust_score")
-    private Long trustScore;
+    /**
+     * 변경날짜
+     */
+    @Column
+    private Date updateDate;
 }
