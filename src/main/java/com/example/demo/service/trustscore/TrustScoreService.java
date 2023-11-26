5e90fc595a4d1a5b7a049d705fc17465185e0747
joo <rkdepik@gmail.com>
2023-11-26 18:05:49 +0900

    [feature/19-trustscore-api] feat : 신뢰점수 관련 Service 개발

--- src/main/java/com/example/demo/service/trustscore/TrustScoreService.java
+++ src/main/java/com/example/demo/service/trustscore/TrustScoreService.java
@@ -1,4 +1,8 @@
 package com.example.demo.service.trustscore;
 
+import com.example.demo.dto.trustscore.request.TrustScoreUpdateRequestDto;
+import com.example.demo.dto.trustscore.response.TrustScoreUpdateResponseDto;
+
 public interface TrustScoreService {
+    TrustScoreUpdateResponseDto updateTrustScore(TrustScoreUpdateRequestDto requestDto);
 }
