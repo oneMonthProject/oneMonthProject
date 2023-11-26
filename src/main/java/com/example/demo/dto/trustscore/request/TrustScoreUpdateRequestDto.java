77f0a7732078ab08e4aa5de1c539852467ecdf0f
joo <rkdepik@gmail.com>
2023-11-26 18:51:04 +0900

    [feature/19-trustscore-api] fix : 접근지정자 변경

--- src/main/java/com/example/demo/dto/trustscore/request/TrustScoreUpdateRequestDto.java
+++ src/main/java/com/example/demo/dto/trustscore/request/TrustScoreUpdateRequestDto.java
@@ -8,10 +8,10 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 public class TrustScoreUpdateRequestDto {
     @NotNull(message = "사용자값은 필수입니다.")
-    Long userId;
-    Long projectId;
-    Long milestoneId;
-    Long workId;
+    private Long userId;
+    private Long projectId;
+    private Long milestoneId;
+    private Long workId;
     @NotNull(message = "점수타입값은 필수입니다.")
-    Long scoreTypeId;
+    private Long scoreTypeId;
 }
