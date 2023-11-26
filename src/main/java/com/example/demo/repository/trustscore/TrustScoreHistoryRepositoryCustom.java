d65fc5cdcad57d4a7b892793d0b9bf68c4f7a796
joo <rkdepik@gmail.com>
2023-11-26 18:06:24 +0900

    [feature/19-trustscore-api] feat : 신뢰점수 이력 관련 Repository 개발

--- src/main/java/com/example/demo/repository/trustscore/TrustScoreHistoryRepositoryCustom.java
+++ src/main/java/com/example/demo/repository/trustscore/TrustScoreHistoryRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.example.demo.repository.trustscore;
 
+import com.example.demo.dto.trustscorehistory.ProjectUserHistoryDto;
+
+import java.util.List;
+
 public interface TrustScoreHistoryRepositoryCustom {
+    List<ProjectUserHistoryDto> getProjectUserHistory(Long projectId, Long userId);
 }
